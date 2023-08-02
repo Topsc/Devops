8b5306e7be251e9fb4477b4adc49e397ccbfe399
Jiayuan <107369285+JaronFang@users.noreply.github.com>
2023-08-03 01:24:20 +1000

    Delete opensearch directory

--- opensearch/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "OPENSEARCH_URL" {
-  description = "OPENSEARCH_URL"
-  value       = module.opensearch_cluster.OPENSEARCH_URL
-}
-
-output "role_arn" {
-  description = "arn of the lambda role"
-  value       = module.lambda_role_module.role_arn
-}
