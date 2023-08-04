b8c9a0bd9fa299c60bfb0f258daf048634524d85
Linda Lu <chaolin1984@gmail.com>
2023-08-04 22:44:23 +1000

    update code and trigger eventbridge

--- lambda-apigate/terraform/lambda.tf
+++ lambda-apigate/terraform/lambda.tf
@@ -60,3 +60,24 @@ resource "aws_s3_object" "handler" {
   source = data.archive_file.handler.output_path
   etag   = filemd5(data.archive_file.handler.output_path)
 }
+
+
+resource "aws_cloudwatch_event_rule" "lambda_scheduled_trigger" {
+  name                = "lambda_scheduled_trigger"
+  description         = "Triggers Lambda function at 10 PM Brisbane time daily"
+  schedule_expression = "cron(0 12 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.lambda_scheduled_trigger.name
+  arn       = aws_lambda_function.handler.arn
+  target_id = "hander"
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_foo" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.handler.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.lambda_scheduled_trigger.arn
+}
